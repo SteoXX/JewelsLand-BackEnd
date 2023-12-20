949c6a10927640e74d69b11236d774e8482e9c1f
SteoXX <stefano.garda2004@gmail.com>
2023-12-20 11:43:14 +0100

    fixes

--- routes/addProducts.js
+++ routes/addProducts.js
@@ -3,6 +3,7 @@ const { Product } = require("../collections");
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
+const User = require("../collections");
 
 const router = express.Router();
 const upload = multer({ dest: path.join(__dirname, "../images") });
@@ -10,6 +11,24 @@ const upload = multer({ dest: path.join(__dirname, "../images") });
 router.post("/", upload.single("image"), async (req, res) => {
   const { name, description, price, stock, category } = req.body;
   const image = req.file;
+
+  const userId = req.session.userId;
+  const user = await User.findById(userId);
+
+  // Check if the user exists
+  if (!user) {
+    res.json({ message: "User not found", status: "UserNotFound" });
+    return;
+  }
+
+  // Check if the user is an admin
+  if (!user.admin) {
+    res.json({
+      message: "You don't have permission to do that",
+      status: "NoPermission",
+    });
+    return;
+  }
 
   // Convert image to base64
   const imgData = fs.readFileSync(image.path).toString("base64");
