abc7499736bb6e503b9e46650135045a7d2a6da8
SteoXX <stefano.garda2004@gmail.com>
2023-12-27 11:42:45 +0100

    started working on stripe

--- routes/displayProducts.js
+++ routes/displayProducts.js
@@ -4,7 +4,50 @@ const { Product } = require("../collections");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const products = await Product.find();
+  let category = req.query.category;
+  let searchContent = req.query.searchContent;
+
+  let products;
+
+  if (category) {
+    // Change the category to handle the database standards
+    category = category.toLowerCase();
+
+    // If a category is provided, find products of that category
+    products = await Product.find({ category: category });
+  } else if (searchContent) {
+    // Change the searchContent to handle the database standards
+    searchContent = searchContent.toLowerCase();
+
+    // If search content is provided, find products containing the search content
+    // \\b to search for the full word and not only a part of it
+    // \\b(?=\\s|$) to eliminate the . or , from the search
+    products = await Product.find({
+      $or: [
+        {
+          name: {
+            $regex: "\\b" + searchContent + "\\b(?=\\s|$)",
+            $options: "i",
+          },
+        },
+        {
+          description: {
+            $regex: "\\b" + searchContent + "\\b(?=\\s|$)",
+            $options: "i",
+          },
+        },
+        {
+          category: {
+            $regex: "\\b" + searchContent + "\\b(?=\\s|$)",
+            $options: "i",
+          },
+        },
+      ],
+    });
+  } else {
+    // If no category or search content is provided, find all products
+    products = await Product.find();
+  }
 
   if (!products) {
     console.log("No products found");
